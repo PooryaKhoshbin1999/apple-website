a0eaa9c0349d2685e86ea97e86764fee315de16e
Mr Poorya Khoshbin <www.pooryakhoshbin@gmail.com>
2025-01-25 16:50:44 +0430

    the initial commit of this site

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: '#2997FF',
+        gray: {
+          DEFAULT: '#86868B',
+          100: '#94928B',
+          200: '#AFAFAF',
+          300: '#42424570',
+        },
+        zinc: '#101010',
       },
     },
   },
